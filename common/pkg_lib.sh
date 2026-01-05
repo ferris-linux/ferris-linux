e91bb9f101c70ea185ded4f3f2c867c82b4010ed
Giorgi Grigalashvili <71205861+BUGO07@users.noreply.github.com>
2026-01-05 13:52:22 +0400

    skip building a package if it's already built

--- common/pkg_lib.sh
+++ common/pkg_lib.sh
@@ -1,17 +1,35 @@
 
 # $1 - destdir, $2 - url
 start_installation() {
-    wget "$2"
     archive_name="$(basename $2)"
-    dir_name=$(tar -tf "$archive_name" | head -1 | cut -f1 -d"/")
-    tar -xf "$archive_name"
     current_dir="$(realpath .)"
+
+    if [ -f "$archive_name" ]; then
+        echo "$archive_name already exists. Skipping build."
+    else
+        echo "Downloading archive $archive_name from $2..."
+        wget "$2"
+
+        echo "Extracting archive $archive_name..."
+        dir_name=$(tar -tf "$archive_name" | head -1 | cut -f1 -d"/")
+        tar -xf "$archive_name"
+        dirnamerealpath="$(realpath $dir_name)"
+        cd "$dirnamerealpath"
+
+        echo "Running configure step..."
+        "configure" "$1"
+
+        echo "Running build step..."
+        "build" "$1"
+
+        cd "$current_dir"
+    fi
+
+    echo "Running install step..."
+    dir_name=$(tar -tf "$archive_name" | head -1 | cut -f1 -d"/")
     dirnamerealpath="$(realpath $dir_name)"
     cd "$dirnamerealpath"
-    "configure" "$1"
-    cd "$dirnamerealpath"
-    "build" "$1"
-    cd "$dirnamerealpath"
+
     "install" "$1"
     cd "$current_dir"
 }
